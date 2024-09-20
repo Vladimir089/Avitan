875ea7b7ada840d137ab5659be82c514600c0bd3
Vladimir089 <pickerpikcer@gmail.com>
2024-09-20 15:17:06 +0300

    Initial Commit

---
+++ Avitan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Avitan
+//
+//  Created by Владимир Кацап on 20.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
