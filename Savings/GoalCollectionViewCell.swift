3ae6c1ffbea8980edd0740587be41b494e5e43ac
Vladimir089 <pickerpikcer@gmail.com>
2024-09-25 15:33:04 +0300

    234

---
+++ Savings/GoalCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  GoalCollectionViewCell.swift
+//  Avitan
+//
+//  Created by Владимир Кацап on 25.09.2024.
+//
+
+import UIKit
+
+class GoalCollectionViewCell: UICollectionViewCell {
+    
+}
